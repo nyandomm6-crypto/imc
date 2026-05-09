ec9dd54b8dc13976fef63f46157b1898038f6474
Raherinjato Ny Ando Linah <nyandomm6@gmail.com>
2026-05-09 10:14:25 +0300

    connection database

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,22 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function dbTest(): string
+    {
+        $db = \Config\Database::connect();
+
+        try {
+            $db->initialize();
+            $result = $db->query('SELECT 1 AS ok')->getRowArray();
+        } catch (\Throwable $e) {
+            return 'Database error: ' . $e->getMessage();
+        }
+
+        if (!isset($result['ok'])) {
+            return 'Database error: unexpected response.';
+        }
+
+        return 'Database OK';
+    }
 }
