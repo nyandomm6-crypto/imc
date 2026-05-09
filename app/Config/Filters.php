f57fac557090beff92bb5658f4555b30b5088a21
Raherinjato Ny Ando Linah <nyandomm6@gmail.com>
2026-05-10 01:27:04 +0300

    d

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -35,6 +35,7 @@ class Filters extends BaseFilters
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
         'auth'          => 'App\\Filters\\AuthFilter',
+        'admin'         => 'App\\Filters\\AdminFilter',
     ];
 
     /**
